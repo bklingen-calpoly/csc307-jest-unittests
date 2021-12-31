80c2a5a9337ac624bbfae4861a75e3fba5dbb0f5
BJ Klingenberg <bklingen@calpoly.edu>
2021-12-31 09:19:04 -0800

    add div and contains numbers

--- sample-functions.js
+++ sample-functions.js
@@ -2,4 +2,21 @@ function sum(a, b) {
   return a + b;
 }
 
+exports.div = function div(a, b) {
+  if (b === 0) {
+    throw new Error("Div by zero.");
+  }
+  return a / b; //or just return this line and expect Infinity
+};
+
+exports.containsNumbers = function containsNumbers(text) {
+  for (let i = 0; i < text.length; i++) {
+    if (!isNaN(text.charAt(i))) return true;
+  }
+  return false;
+};
+
+//Also, note different ways to export functions.
 exports.sum = sum;
+// exports.div = div;
+// exports.containsNumbers = containsNumbers;
--- sample-functions.test.js
+++ sample-functions.test.js
@@ -1,7 +1,74 @@
 const myFunctions = require("./sample-functions.js");
 
+//Setup functions
+// beforeEach( () => {
+
+// });
+
+// beforeAll( () => {
+
+// });
+
+// //Teardown functions
+// afterEach( () => {
+
+// });
+
+// afterAll( () => {
+
+// });
+
 test("Testing sum -- success", () => {
   const target = 30;
   const result = myFunctions.sum(12, 18);
-  expect(target).toBe(result);
+  expect(result).toBe(target);
 });
+
+test("all letters", () => {
+  const result = myFunctions.containsNumbers("as;ldk");
+  expect(result).toBeFalsy();
+});
+
+test("lettters and one number in the middle", () => {
+  const result = myFunctions.containsNumbers("as;l8dk");
+  expect(result).toBeTruthy();
+});
+
+test("lettters and at least one whitespace", () => {
+  const result = myFunctions.containsNumbers("as; dk");
+  expect(result).toBeFalsy();
+});
+
+test("last number", () => {
+  const result = myFunctions.containsNumbers("as;ldk8");
+  expect(result).toBeTruthy();
+});
+// test.only('div by zero with infinity', ()=>{
+//     expect(myFunctions.div(23, 0)).toBe(Infinity);
+// });
+
+test("div by zero with Error thrown", () => {
+  expect(() => myFunctions.div(23, 0)).toThrowError(/Div by zero/);
+});
+
+// test('Testing div by zero', () => {
+//     expect(myFunctions.div(10,0)).toBe(Infinity);
+// });
+
+// test('Testing div by zero', () => {
+//     expect(() => myFunctions.div(10,0)).toThrow(/Div by zero not supported/);
+// });
+
+//https://jestjs.io/docs/setup-teardown
+
+//Runs before each test case
+beforeEach(() => {});
+
+//Runs after each case
+afterEach(() => {});
+
+//Runs once before all test cases start
+beforeAll(() => {});
+
+//Runs once after all test cases end
+afterAll(() => {});
